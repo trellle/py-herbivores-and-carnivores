b446018f23ea2f6208b86abd826d36297740b698
yarik <super.racing@ukr.net>
2025-06-27 12:49:27 +0300

    created class Animal with subclasses

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return ("{"
+                + f"Name: {self.name}, "
+                + f"Health: {self.health}, "
+                + f"Hidden: {self.hidden}"
+                + "}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(victim: Herbivore | Carnivore) -> None:
+        if type(victim) == Herbivore and not victim.hidden:
+            victim.health -= 50
+        if victim.health <= 0:
+            Animal.alive.remove(victim)
