5f015a4b64afba3890222039b6f55e49d5ea9a39
yarik <super.racing@ukr.net>
2025-06-28 23:44:55 +0300

    rafactored my code

--- app/main.py
+++ app/main.py
@@ -22,10 +22,7 @@ class Animal:
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        if self.hidden:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
